767b51db1146f7448bef9df23dffa1824bf84dc7
Seth T <seth10@users.noreply.github.com>
2025-04-17 18:13:51 +0000

    Create NeoPixelConfig and allow for independent control
    Also allow for 4 stages of brightness

--- ble_control.py
+++ ble_control.py
@@ -1,7 +1,11 @@
 """
 This sketch uses the free Adafruit Bluefruit LE Connect App. Download and open the app, check the "Must have UART Service" filter, and tap the "Connect" button next to the one device named something like "CIRCUITPYc11325".
-Then navigate to Controller > Control Pad, and tap 1 to turn on the lights on both twintail holders, and 2 to turn the lights off.
+Then navigate to Controller > Control Pad, and use buttons 1-4 to control the brightness. 0: 0% (off), 1: 30%, 2: 70%, 3: 100%.
 """
+
+import asyncio
+import board
+import neopixel
 
 from adafruit_ble import BLERadio
 from adafruit_ble.advertising.standard import ProvideServicesAdvertisement
@@ -10,24 +14,30 @@ from adafruit_ble.services.nordic import UARTService
 from adafruit_bluefruit_connect.packet import Packet
 from adafruit_bluefruit_connect.button_packet import ButtonPacket
 
-import asyncio
-import board
-import neopixel
-
-# Which pins the data lines of the NeoPixel strips are each connected to
-NEOPIXEL_PIN1 = board.A0
-NEOPIXEL_PIN2 = board.A1
-# How many LED are tucked in the back of the twintail holder, not visible
-LEDS_TO_SKIP = 0
-# How many LEDs are actually part of the accessory
-LEDS_IN_LOOP = 156
 # Display constants
-BRIGHTNESS = 0.8
+INITIAL_BRIGHTNESS = 0.8
 RED = (255, 0, 0)
 BLACK = (0, 0, 0)
 
-pixels1 = neopixel.NeoPixel(NEOPIXEL_PIN1, LEDS_TO_SKIP + LEDS_IN_LOOP, brightness=BRIGHTNESS)
-pixels2 = neopixel.NeoPixel(NEOPIXEL_PIN2, LEDS_TO_SKIP + LEDS_IN_LOOP, brightness=BRIGHTNESS)
+class NeoPixelConfig:
+    # Which pins the data lines of the NeoPixel strips are each connected to
+    data_pin: Pin
+    # How many LED are tucked in the back of the twintail holder, not visible
+    leds_to_skip: int
+    # How many LEDs are actually part of the accessory
+    leds_in_loop: int
+    
+    def __init__(self, data_pin, leds_to_skip, leds_in_loop):
+        self.data_pin = data_pin
+        self.leds_to_skip = leds_to_skip
+        self.leds_in_loop = leds_in_loop
+
+# index 8 is the first LED I want to light up, 153 is the last one that exists, the usable loop is 146 long
+left_cfg = NeoPixelConfig(data_pin=board.A0, leds_to_skip=7, leds_in_loop=146)
+# index 4 is the first LED I want to light up, 149 is the last one that exists, the usable loop is 146 long
+right_cfg = NeoPixelConfig(data_pin=board.A1, leds_to_skip=3, leds_in_loop=146)
+left = neopixel.NeoPixel(left_cfg.data_pin, left_cfg.leds_to_skip + 1 + left_cfg.leds_in_loop, brightness=INITIAL_BRIGHTNESS, auto_write=False)
+right = neopixel.NeoPixel(right_cfg.data_pin, right_cfg.leds_to_skip + 1 + right_cfg.leds_in_loop, brightness=INITIAL_BRIGHTNESS, auto_write=False)
 
 ble = BLERadio()
 uart = UARTService()
@@ -36,11 +46,18 @@ advertisement = ProvideServicesAdvertisement(uart)
 class Controls:
     def __init__(self):
         self.color = RED
+        self.brightness = INITIAL_BRIGHTNESS
 
 async def animate_neopixels(controls):
     while True:
-        pixels1.fill(controls.color)
-        pixels2.fill(controls.color)
+        for i in range(0, left_cfg.leds_in_loop):
+            left[left_cfg.leds_to_skip + 1 + i] = controls.color
+        for i in range(0, right_cfg.leds_in_loop):
+            right[right_cfg.leds_to_skip + 1 + i] = controls.color
+        left.brightness = controls.brightness
+        right.brightness = controls.brightness
+        left.show()
+        right.show()
         await asyncio.sleep(0.1)
 
 async def monitor_ble_control_pad(controls):
@@ -55,9 +72,13 @@ async def monitor_ble_control_pad(controls):
                 if isinstance(packet, ButtonPacket):
                     if packet.pressed:
                         if packet.button == ButtonPacket.BUTTON_1:
-                            controls.color = RED
+                            controls.brightness = 0.0
                         elif packet.button == ButtonPacket.BUTTON_2:
-                            controls.color = BLACK
+                            controls.brightness = 0.3
+                        elif packet.button == ButtonPacket.BUTTON_3:
+                            controls.brightness = 0.7
+                        elif packet.button == ButtonPacket.BUTTON_4:
+                            controls.brightness = 1.0
             await asyncio.sleep(0)
 
         # If we got here, we lost the connection. Go up to the top of this function and start advertising again, waiting for a connection.
